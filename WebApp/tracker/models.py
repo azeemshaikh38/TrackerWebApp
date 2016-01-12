c3b73521fa0b2901873054f15a663ec8a2ff8dd5
azeem <azeem@getexp.com>
2016-01-12 02:14:30 +0000

    Tracker models v1

--- WebApp/tracker/models.py
+++ WebApp/tracker/models.py
@@ -1,7 +1,81 @@
 from __future__ import unicode_literals
-
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Activi
+class Activities(model.Model):
+    name = models.CharField(max_length=64)
+    description = models.CharField(max_length=255, null=True)
+    class Meta:
+        db_table = "Activities"
+
+class Subfields(models.Model):
+    field = models.ForiegnKey(Fields)
+    name = models.CharField(max_length=64)
+    description = models.CharField(max_length=255, null=True)
+    class Meta:
+        db_table = "Subfields"
+
+class Fields(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.CharField(max_length=255, null=True)
+    class Meta:
+        db_table = "Fields"
+
+class ActivityFieldEffects(models.Model):
+    activity = models.ForeignKey(Activities)
+    field = models.ForiengKey(Fields)
+    effect = models.FloatField()
+    class Meta:
+        db_table = "ActivityFieldEffects"
+
+class ActivitySubfieldEffects(models.Model):
+    activity = models.ForeignKey(Activities)
+    subfield = models.ForeignKey(SubFields)
+    effect = models.FloatField()
+    class Meta:
+        db_table = "ActivitySubfieldEffects"
+
+class UserActivities(models.Model):
+    user = models.ForeignKey(User)
+    activity = models.ForeignKey(Activities)
+    class Meta:
+        db_table = "UserActivities"
+
+class UserFields(models.Model):
+    user = models.ForeignKey(User)
+    field = models.ForeignKey(Fields)
+    score = models.FloatField()
+    class Meta:
+        db_table = "UserFields"
+
+class UserSubfields(models.Model):
+    user = models.ForeignKey(User)
+    subfield = models.ForeignKey(Subfields)
+    score = models.FloatField()
+    class Meta:
+        db_table = "UserSubfields" 
+
+#class UserActivityLogs(models.Model):
+#    date = models.DateField()
+#    activity = models.ForeignKey(Activities)
+#    user = models.ForeignKey(User)
+#    class Meta:
+#        db_table = "UserActivityLogs"
+#
+#class UserFieldProgress(models.Model):
+#    date = models.DateField()
+#    user = models.ForeignKey(User)
+#    field = models.ForeignKey(Fields)
+#    score = models.FloatField()
+#    class Meta:
+#        db_table = "UserFieldProgress"
+#    
+#class UserSubfieldProgress(models.Model):
+#    date = models.DateField()
+#    user = models.ForeignKey(User)
+#    field = models.ForeignKey(Subfields)
+#    score = models.FloatField()
+#    class Meta:
+#        db_table = "UserSubfieldProgress"
